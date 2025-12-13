570ba6313a3c9a9e955e435a164598bc9367533b
Abhishek <bhab16747@gmail.com>
2025-12-13 11:25:02 +0530

    Initial commit for Maven JavaFX project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+import java.sql.*;
+
+@SuppressWarnings("unused")
+
+
+
+
+public class App  {
+
+
+
+}
