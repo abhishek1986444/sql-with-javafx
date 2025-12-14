a18d0be57f97818c5163680b7bc2b1a499159c40
Abhishek <bhab16747@gmail.com>
2025-12-14 11:06:17 +0530

    only fetching input

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,8 +8,20 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import java.util.Properties;
+
+
 
 import java.sql.*;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+import java.util.Vector;
+
+
 
 @SuppressWarnings("unused")
 
@@ -18,6 +30,89 @@ import java.sql.*;
 
 public class App  {
 
+public static void main (String [] args ) throws Exception
+{
+DBConnection object = new DBConnection();
 
 
+
+struct admin = object.User_Connection();
+
+Statement stmt = admin.connection.createStatement();
+
+ResultSet rs = stmt.executeQuery("Select * from " + admin.master_Table + "  ;  ");
+
+ResultSetMetaData rsmd =  rs.getMetaData();
+
+
+System.out.println(rsmd.getColumnCount());
+
+
+
+}
+
+
+
+}
+
+
+
+
+
+class struct {
+
+Connection connection;
+
+String master_Table;
+
+
+
+
+
+}
+
+
+
+
+class DBConnection{
+    public struct User_Connection() throws Exception
+    {
+        struct object = new struct();
+
+       
+            Properties props = new Properties();
+
+
+            FileInputStream  fis = new FileInputStream(
+    "C:/Users/ABHISHEK/Downloads/deadnote/version_2/withjavafx/db.properties"
+            );
+        
+         props.load(fis);
+
+
+       String url = props.getProperty("db.url");
+
+      String driver = props.getProperty("db.driver");
+
+     String password = props.getProperty("db.password");
+        String user = props.getProperty("db.user");
+
+
+
+                     
+                     
+ object.master_Table = props.getProperty("db.master_db_table");
+
+ Class.forName(driver);
+
+ object.connection = DriverManager.getConnection(url,user,password);
+
+
+ System.out.println("Connection successful!");
+
+
+        return object;
+
+
+    }
 }
